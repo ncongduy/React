914f44edb48ebc9ac5768d856e418dc0c2a50a20
Nguyen Cong Duy <ncongduy@gmail.com>
2020-10-16 20:30:29 +0300

    project 3: ravenous (part 4 - add API key)

--- Project 3 - Ravenous (Build Components)/ravenous/src/components/BusinessList/BusinessList.js
+++ Project 3 - Ravenous (Build Components)/ravenous/src/components/BusinessList/BusinessList.js
@@ -6,14 +6,12 @@ class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
-        {this.props.businesses.map(
-          (business) => {
-            return <Business business={business} />;
-          }
-        )}
+        {this.props.businesses.map((business) => {
+          return <Business business={business} key={business.id} />;
+        })}
       </div>
     );
   }
-};
+}
 
 export default BusinessList;
